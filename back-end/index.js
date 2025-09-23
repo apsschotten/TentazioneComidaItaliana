3922ac98f6c2eff58e950d6ec4ad7479d2386984
Hner1qwe <henriquetrachtafilho@gmail.com>
2025-09-23 17:13:23 -0400

    index update

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const app = express();
 
+const usuarioController = require('../back-end/controllers/usuarioControllers');
+
+const mesaControllers = require("../back-end/controllers/mesaControllers")
+
+const mesaRoutes = require("../back-end/routes/mesaRoutes")
+app.use("/mesa", mesaControllers);
+
+const usuarioRoutes = require("../back-end/routes/usuarioRoutes");
+app.use("/usuario", usuarioRoutes);
+
 app.use(express.urlencoded({ extends: true }));
 app.use(express.json());
 

2d7a6a0e9216103c9a9e3fcbab27c9917c0fed88
Hner1qwe <henriquetrachtafilho@gmail.com>
2025-10-01 14:46:18 -0400

    final

--- back-end/routes/usuarioRoutes.js
+++ back-end/routes/usuarioRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuarioControllers');
 
 router.post("/cadastrar", usuarioController.cadastrar);
+router.post("/login", usuarioController.login)
 
 
 module.exports = router;
